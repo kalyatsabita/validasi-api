1ef1a32ad1d67d7f760c323a3b462756a49dd4b5
kalyatsabita <titakalya@gmail.com>
2023-10-07 21:35:55 +0700

    update validasi-api-tugas

--- index.js
+++ index.js
@@ -1,104 +1,163 @@
 const express = require("express")
 const {body, validationResult} = require("express-validator")
-const errorHandlerMiddleware = require('./middlewares/error-handler')
-const registerValidator = require('./middlewares/register-validator')
-// const loginValidator = require('./middlewares/login-validator')
-
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const {param} = require("express-validator")
 const app = express()
+const secretKey = 'mysecretkey'
 
 app.use(express.json())
 
-// app.get("/", (req, res) => {
-//     // next
-//     // ini namanya middleware
-//     // console.log("Hello world")
-//     // next()
-//     return res.status(200).json({
-//         status: "success",
-//         message: "Selamat datang di API belajar validation"
-//     })
-// })
+let users = [];
+let userIdCounter = 1;
 
-app.post("/auth/register",
-            // body("email")
-            //     .notEmpty().withMessage("Email Wajib Diisi")
-            //     .bail()
-            //     .custom(value => value.endsWith('@gmail.com'))
-            //     .withMessage("Email harus menggunakan domain @gmail.com")
-            //     .bail()
-            //     .isEmail().withMessage("Email Tidak Valid"),
-            // body("password")
-            //     .notEmpty().withMessage("Password Wajib Diisi")
-            //     .isLength({ min: 8 })
-            //     .withMessage("Password Minimal 8 Karakter"),
-            // body("ttl")
-            //     .notEmpty().withMessage("Tanggal Lahir Wajib Diisi")
-            //     .isDate({ format: 'YYYY-MM-DD' }).withMessage("Format Tanggal Tidak Valid (YYYY-MM-DD)"),
-            // body("bio")
-            //     .notEmpty().withMessage("Bio Wajib Diisi")
-            //     .isLength({ min: 30, max: 999 })
-            //     .withMessage("Bio Minimal 30 Karakter dan Maksimal 999 Karakter"),
-            // body("hobi")
-            //     .optional()
-            //     .isString().withMessage("Hobi Wajib Wajib Berupa String"),
-            // body("pendidikan")
-            //     .optional()
-            //     .isArray().withMessage("Pendidikan Wajib Berupa Array")
-            //     .bail()
-            //     .custom(values => {
-            //         if (values.length > 0) {
-            //             return values.every(value => ["SD", "SMP", "SMA", "SMK", "S1", "S2", "S3"].includes(value));
-            //         }
-            //         return true;
-            //     }).withMessage("Pendidikan hanya Boleh: SD, SMP, SMA, SMK, S1, S2, S3"),
-        registerValidator, (req, res) => {
-    // registerValidator,    
-    validationResult(req).throw()
-    // console.log(req.body)
-    //     res.json({ message: "test register" })
-    // const validateResult = validationResult(req);
-    // if(!validateResult.isEmpty()) {
-    //     return res.status(400).json({
-    //         status: "failed",
-    //         message: "Validation Error",
-    //         error: validateResult.array()
-    //     })
-    // }
-    const body = req.body;
-    return res.status(200).json({
-        status: "success",
-        message: "Berhasil Register",
-        data: body
+app.post('/auth/register', [
+    body('fullName')
+        .notEmpty().withMessage('Nama Wajib Diisi'),
+    body('email')
+        .notEmpty().withMessage('Email Wajib Diisi')
+        .isEmail().withMessage('Email Tidak Valid'),
+    body('password')
+        .notEmpty().withMessage('Password Wajib Diisi')
+        .isLength({ min: 8 }).withMessage('Password Minimal 8 Karakter')
+        .matches(/[\W]/).withMessage('Password Wajib Memiliki Minimal 1 Simbol'),
+    body('bio')
+        .optional(),
+    body('dob')
+        .notEmpty().withMessage('Date of Birth Wajib Diisi')
+        .isDate({ format: 'YYYY-MM-DD' }).withMessage('Format Tanggal Tidak Valid (YYYY-MM-DD)'),
+    ], (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            message: 'Validation Error', 
+            detail: errors.array()
+        })
+    }
+
+    const { fullName, email, password, bio, dob } = req.body;
+
+    if (users.some(user => user.email === email)) {
+        return res.status(400).json({
+            message: 'Email Sudah Pernah Terdaftar.'
+        })
+      }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const user = {
+        id: userIdCounter++,
+        fullName,
+        email,
+        password: hashedPassword,
+        bio,
+        dob
+    }
+
+    users.push(user);
+
+    res.status(201).json({
+        message: 'Registrasi Success'
     })
-})
+});
 
-// app.post("/auth/login", loginValidator, (req, res) => {
-//     loginValidator,
-//         // const result = validationResult(req);
-//         // if (!result.isEmpty()) {
-//         //     // validasi gagal
-//         //     return res.status(400).json({ message: "validation error", detail: result.array() })
-//         // }
-    // validationResult(req).throw()
-//         console.log(req.body)
-//         res.json({ message: "test login" })
-    // const validateResult = validationResult(req);
-    // if(!validateResult.isEmpty()) {
-    //     return res.status(400).json({
-    //         status: "failed",
-    //         message: "Validation Error",
-    //         error: validateResult.array()
-    //     })
-    // }
-//     const body = req.body;
-//     return res.status(200).json({
-//         status: "success",
-//         message: "Berhasil Login",
-//         data: body
-//     })
-// })
+app.post('/auth/login', [
+    body('email')
+        .notEmpty().withMessage('Email Wajib Diisi')
+        .isEmail().withMessage('Email Tidak Valid'),
+    body('password')
+        .notEmpty().withMessage('Password Wajib Diisi')
+        .isLength({ min: 8 }).withMessage('Password Minimal 8 Karakter')
+        .matches(/[\W]/).withMessage('Password Wajib Memiliki Minimal 1 Simbol'),
+    ], (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+            message: 'Validation Error',
+            detail: errors.array()
+        })
+    }
+  
+    const { email, password } = req.body;
+  
+    const user = users.find(user => user.email === email);
+    if (!user) {
+      return res.status(401).json({
+            message: 'Login Failed'
+        })
+    }
+  
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+            message: 'Login Failed'
+        })
+    }
 
-app.use(errorHandlerMiddleware)
+    const token = jwt.sign({ id: user.id, email: user.email }, secretKey);
+  
+    res.status(200).json({
+        message: 'Success',
+        data: { token }
+    })
+});
+
+app.get('/users', (req, res) => {
+    if (users.length === 0) {
+      return res.status(404).json({
+            message: 'User Not Found'
+        })
+    }
+  
+    const userData = users.map(user => ({
+      fullName: user.fullName,
+      email: user.email,
+      bio: user.bio,
+      dob: user.dob
+    }))
+  
+    res.status(200).json({
+        message: 'Success',
+        data: userData
+    })
+});
+  
+app.get('/users/:userId', [
+    param('userId')
+        .isNumeric().withMessage('User ID Wajib Berbentuk Angka'),
+    ], (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+            message: 'Validation Error',
+            detail: errors.array()
+        })
+    }
+  
+    const userId = parseInt(req.params.userId, 10);
+    const user = users.find(user => user.id === userId);
+  
+    if (!user) {
+      return res.status(404).json({
+            message: 'User Not Found' 
+        })
+    }
+  
+    const userData = {
+      fullName: user.fullName,
+      email: user.email,
+      bio: user.bio,
+      dob: user.dob
+    }
+  
+    res.status(200).json({
+        message: 'Success',
+        data: userData
+    })
+});
 
 app.listen(1945, ()=> {
     console.log(`app start at http://localhost:1945`)
